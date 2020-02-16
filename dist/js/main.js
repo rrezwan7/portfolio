d1bf664069bba62ea7f0902daf73473b9f811616
Rezwan <rezwan.storerepublic@gmail.com>
2020-02-16 17:29:52 +0600

    Final Commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
-const humburger = document.querySelector('.menu-btn__burger');
-
+const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
 
 let showMenu = false;
 
@@ -10,14 +11,17 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     if (!showMenu) {
-        humburger.classList.add('open');
+        hamburger.classList.add('open');
         nav.classList.add('open');
         menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
         showMenu = true;
+
     } else {
-        humburger.classList.remove('open');
+        hamburger.classList.remove('open');
         nav.classList.remove('open');
         menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
         showMenu = false;
     }
 }
